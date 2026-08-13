3fa5160736b040b47d3dd9f38d435713bfbbb844
Gojo117 <loymoney8@gmail.com>
2026-08-13 15:44:29 +0700

    update code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-with-database-production.up.railway.app/api'
+  apiUrl: 'https://api-with-database-production.up.railway.app'
 };
